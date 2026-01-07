de7cec702f533111003c227e29904df23097566c
piouoiq <melargilliere@gmail.com>
2026-01-07 16:20:15 +0100

    chore : homePage section1

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,16 @@
 export default function App() {
   return (
     <>
+      <section className=" text-center text-primary-color py-40 bg-[url(../public/images/background.jpg)] bg-cover bg-center brightness-80 font-family-heading">
+        <h1 className="text-6xl pb-12">
+          Donnons-leurs autant qu'ils nous apportent
+        </h1>
+        <p>
+          Chaque jour, des milliers d'animaux attendent une famille aimante.
+          Trouvez votre <br />
+          compagnon idéal parmi nos animaux disponibles à l'adoption.
+        </p>
+      </section>
     </>
   );
 }
