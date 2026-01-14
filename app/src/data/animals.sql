c1ba3e7c2d07bfcee22bd6c854673be71727936d
piouoiq <melargilliere@gmail.com>
2026-01-14 10:25:09 +0100

    feat tables volunteer & types

--- app/src/data/animals.sql
+++ app/src/data/animals.sql
@@ -11,3 +11,23 @@ CREATE TABLE animals (
     FOREIGN KEY (type_id) REFERENCES types(id),
     FOREIGN KEY (city_id) REFERENCES city(id)
 );
+
+CREATE TABLE types (
+    id INTEGER PRIMARY KEY,
+    types TEXT NOT NULL
+);
+
+CREATE TABLE volunteers (
+    id INTEGER PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    birth_date DATE,
+    created_at DATETIME,
+    disponibilites ENUM ('morning', 'afternoon', 'evening'),
+    city_id BIGINT,
+
+    FOREIGN KEY (city_id) REFERENCES city(id)
+
+
+);
